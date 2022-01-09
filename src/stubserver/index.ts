820fa7c6107f8d9c26ee32ab9ffc683c13166cb4
sooyoung-cho <sooyoung.cho@mymusictaste.com>
2022-01-09 10:20:30 +0900

    feat: Add the postive case of static Cached API server

---
+++ src/stubserver/index.ts
@@ -0,0 +1,37 @@
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
+import { CACHE_ROOT } from '../constants';
+import { readFile } from 'fs/promises';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const prefix = path.join(CACHE_ROOT + '/www_mymusictaste_com');
+const getCache = async (req: Request) => {
+  const endpoint = req.params[0];
+  const filename = `GET__${endpoint.split('/').join('_')}.json`;
+  const key = req.originalUrl.replace(req.path, '');
+  const filepath = path.join(prefix, endpoint, `${filename}`);
+
+  const cacheFile = await readFile(filepath, { encoding: 'utf-8' });
+  const rawCache = JSON.parse(cacheFile);
+  const cache = rawCache[key];
+
+  return cache;
+};
+
+app.get('/parrot/*', async (req: Request, res: Response, next) => {
+  const cache = await getCache(req);
+  res.json(cache);
+  next();
+});
+app.get('*', (req, res) => {
+  res.send('Invalid request');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
